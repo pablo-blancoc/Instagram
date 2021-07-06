9da936db977accf0c2651f1fda5d5647a2766c43
Pablo Blanco <pabloblanco@fb.com>
2021-07-06 16:58:35 -0500

    implemented toolbar and menu to logout

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Constants
+    private static final String TAG = "MainActivity";
+
+    // Attributes
+    private Toolbar toolbar;
+    private MenuItem loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup toolbar
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.toolbar.setTitle("Instagram");
+        setSupportActionBar(this.toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.feed_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        loading = menu.findItem(R.id.progressBar);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if( id == R.id.logout ) {
+            this.onLogout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onLogout() {
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if( currentUser == null ) {
+            Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+            // With finish the MainActivity is removed from back-stack
+            finish();
+        } else {
+            Toast.makeText(this, "An error ocurred", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Could not log out");
+        }
     }
 }
